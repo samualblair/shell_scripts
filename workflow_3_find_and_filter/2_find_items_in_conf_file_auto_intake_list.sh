9967c8d3248148ed7ccd280ae00989820eed2127
samualblair <10884846+samualblair@users.noreply.github.com>
2025-05-07 09:37:45 -0700

    changed grep command to input parsing to

--- workflow_3_find_and_filter/2_find_items_in_conf_file_auto_intake_list.sh
+++ workflow_3_find_and_filter/2_find_items_in_conf_file_auto_intake_list.sh
@@ -26,7 +26,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_va.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_va.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_va.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_va.txt"
     echo "##########" >> "${f5_conf_file}_check_va.txt"
   done < "${f5_conf_file}_va.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_va.txt"
@@ -34,7 +34,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_vs.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_vs.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_vs.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_vs.txt"
     echo "##########" >> "${f5_conf_file}_check_vs.txt"
   done < "${f5_conf_file}_vs.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_vs.txt"
@@ -42,7 +42,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_pool.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_pool.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_pool.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_pool.txt"
     echo "##########" >> "${f5_conf_file}_check_pool.txt"
   done < "${f5_conf_file}_pool.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_pool.txt"
@@ -50,7 +50,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_node.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_node.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_node.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_node.txt"
     echo "##########" >> "${f5_conf_file}_check_node.txt"
   done < "${f5_conf_file}_node.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_node.txt"
@@ -58,7 +58,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_monitor.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_monitor.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_monitor.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_monitor.txt"
     echo "##########" >> "${f5_conf_file}_check_monitor.txt"
   done < "${f5_conf_file}_monitor.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_monitor.txt"
@@ -66,7 +66,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_irule.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_irule.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_irule.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_irule.txt"
     echo "##########" >> "${f5_conf_file}_check_irule.txt"
   done < "${f5_conf_file}_irule.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_irule.txt"
@@ -74,7 +74,7 @@ do
   echo "###### PARSING START ######" >> "${f5_conf_file}_check_net_route.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_conf_file}_check_net_route.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_conf_file" >> "${f5_conf_file}_check_net_route.txt"
+    grep "$line" < "$f5_conf_file" >> "${f5_conf_file}_check_net_route.txt"
     echo "##########" >> "${f5_conf_file}_check_net_route.txt"
   done < "${f5_conf_file}_net_route.txt"
   echo "###### PARSING END   ######" >> "${f5_conf_file}_check_net_route.txt"
@@ -91,7 +91,7 @@ do
   echo "###### PARSING START ######" >> "${f5_base_conf_file}_check_self.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_base_conf_file}_check_self.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_base_conf_file" >> "${f5_base_conf_file}_check_self.txt"
+    grep "$line" < "$f5_base_conf_file" >> "${f5_base_conf_file}_check_self.txt"
     echo "##########" >> "${f5_base_conf_file}_check_self.txt"
   done < "${f5_base_conf_file}_self.txt"
   echo "###### PARSING END   ######" >> "${f5_base_conf_file}_check_self.txt"
@@ -99,7 +99,7 @@ do
   echo "###### PARSING START ######" >> "${f5_base_conf_file}_check_rd.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_base_conf_file}_check_rd.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_base_conf_file" >> "${f5_base_conf_file}_check_rd.txt"
+    grep "$line" < "$f5_base_conf_file" >> "${f5_base_conf_file}_check_rd.txt"
     echo "##########" >> "${f5_base_conf_file}_check_rd.txt"
   done < "${f5_base_conf_file}_rd.txt"
   echo "###### PARSING END   ######" >> "${f5_base_conf_file}_check_rd.txt"
@@ -107,7 +107,7 @@ do
   echo "###### PARSING START ######" >> "${f5_base_conf_file}_check_vlan.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_base_conf_file}_check_vlan.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_base_conf_file" >> "${f5_base_conf_file}_check_vlan.txt"
+    grep "$line" < "$f5_base_conf_file" >> "${f5_base_conf_file}_check_vlan.txt"
     echo "##########" >> "${f5_base_conf_file}_check_vlan.txt"
   done < "${f5_base_conf_file}_vlan.txt"
   echo "###### PARSING END   ######" >> "${f5_base_conf_file}_check_vlan.txt"
@@ -115,7 +115,7 @@ do
   echo "###### PARSING START ######" >> "${f5_base_conf_file}_check_mgmt_route.txt"
   echo "###### PARSING DATE: $(DATE)" >> "${f5_base_conf_file}_check_mgmt_route.txt"
   while IFS= read -r line; do
-    grep "$line" "$f5_base_conf_file" >> "${f5_base_conf_file}_check_mgmt_route.txt"
+    grep "$line" < "$f5_base_conf_file" >> "${f5_base_conf_file}_check_mgmt_route.txt"
     echo "##########" >> "${f5_base_conf_file}_check_mgmt_route.txt"
   done < "${f5_base_conf_file}_mgmt_route.txt"
   echo "###### PARSING END   ######" >> "${f5_base_conf_file}_check_mgmt_route.txt"
